55f8ce6b02d108f6664b96d2deda936ec4a83ad3
Aditya Morankar <47907947+adi112100@users.noreply.github.com>
2020-03-15 17:00:37 +0530

    Add files via upload

---
+++ lorenz.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lorenz : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float sigma,roh,beta;
+    public float dt = 0.01f;
+    float dx, dy, dz;
+    float x, y, z;
+
+    private void Start()
+    {
+        x = 0.01f;
+        y = 0f;
+        z=0f;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        dx = (sigma * (y - x))*dt;
+        dy = (x * (roh - z) - y)*dt;
+        dz = (x * y - beta * z)*dt;
+
+        x += dx;
+        y += dy;
+        z += dz;
+
+
+        transform.Translate(dx, dy, dz);
+
+    
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+This is code to create amazing design based on Lorenz attractor
+base on value of constants how amzing designs are produce
+
+this code is especially for unity 
+written in c#
